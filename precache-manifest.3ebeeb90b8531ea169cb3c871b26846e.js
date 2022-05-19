b4bbc51359e6d55d89e1d2702278e6090f9c8371
Robert Kottelin <robert.kottelin@gmail.com>
2022-05-19 17:59:31 +0200

    Updates

---
+++ precache-manifest.3ebeeb90b8531ea169cb3c871b26846e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8f348a3ec1e008be777",
+    "url": "/BrainDump/static/css/main.ce12889c.chunk.css"
+  },
+  {
+    "revision": "f8f348a3ec1e008be777",
+    "url": "/BrainDump/static/js/main.f8f348a3.chunk.js"
+  },
+  {
+    "revision": "73ea5baeb5483e26d2b1",
+    "url": "/BrainDump/static/css/1.da6edbde.chunk.css"
+  },
+  {
+    "revision": "73ea5baeb5483e26d2b1",
+    "url": "/BrainDump/static/js/1.73ea5bae.chunk.js"
+  },
+  {
+    "revision": "30a0e31c29499abf78fe",
+    "url": "/BrainDump/static/js/runtime~main.30a0e31c.js"
+  },
+  {
+    "revision": "44119c8060328481fba564578f3d7420",
+    "url": "/BrainDump/index.html"
+  }
+];
